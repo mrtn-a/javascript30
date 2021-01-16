4179aaa831181f94673a6612d3da42375a51e287
Martyna Markiel <martyna.markiel@gmail.com>
2021-01-16 23:05:12 +0100

    add keyup function to check and compare with secretcode + run cornify_add()

--- 12-key-sequence-detection/index.js
+++ 12-key-sequence-detection/index.js
@@ -1,1 +1,13 @@
-index.js
+const pressed = [];
+const secretCode = 'marty';
+
+window.addEventListener('keyup', (event) => {
+  console.log(event.key);
+  pressed.push(event.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join('').includes(secretCode)) { // join('') -> makes array a string
+    console.log('WooHoo!')
+    cornify_add()
+  }
+  console.log(pressed);
+})
