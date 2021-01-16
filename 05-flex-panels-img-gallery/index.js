77b3c647766c9829b889909dce82e2307fdd347f
Martyna Markiel <martyna.markiel@gmail.com>
2021-01-16 22:33:48 +0100

    add eventlistener toggle open-active class on transitionend

--- 05-flex-panels-img-gallery/index.js
+++ 05-flex-panels-img-gallery/index.js
@@ -4,4 +4,12 @@ function toggleOpen() {
   this.classList.toggle('open');
 }
 
+function toggleActive(event) {
+  console.log(event.propertyName)
+  if(event.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
